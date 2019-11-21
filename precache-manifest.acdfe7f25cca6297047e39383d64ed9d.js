1189794e2ba1ab6dab757d353a42b46de4d88e57
codenation <codenation@codenations-MacBook-Pro.local>
2019-11-21 11:06:10 +0000

    Updates

---
+++ precache-manifest.acdfe7f25cca6297047e39383d64ed9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf0742299aeee027bc93a5a299bfff5",
+    "url": "/react-github/index.html"
+  },
+  {
+    "revision": "39d04d9e6e64f79e2929",
+    "url": "/react-github/static/css/main.c40c5549.chunk.css"
+  },
+  {
+    "revision": "7c4ffcb86e39d16a3da4",
+    "url": "/react-github/static/js/2.dbc928e7.chunk.js"
+  },
+  {
+    "revision": "39d04d9e6e64f79e2929",
+    "url": "/react-github/static/js/main.11ff69e5.chunk.js"
+  },
+  {
+    "revision": "d4bcf24bfbbdb739b82a",
+    "url": "/react-github/static/js/runtime-main.283f58d6.js"
+  }
+]);
